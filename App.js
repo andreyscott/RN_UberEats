1401eade43f5f9db6c93cb3c92133e752f745936
andreyscott <62455784+andreyscott@users.noreply.github.com>
2022-01-13 07:43:15 +0100

    finished the categories component working on the

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-
+import Categories from "./components/Categories";
 import HeaderTabs from "./components/HeaderTabs";
 import SearchBar from "./components/SearchBar";
 
-
 export default function App() {
-  return( 
-  <SafeAreaView  style={{ backgroundColor: "#eee", flex: 1,}}>
-  <View style={{ backgroundColor: "white",  padding: 15}}>
-    <HeaderTabs />
-    <SearchBar  /> 
-    </View>
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+        <Categories />
+      </View>
     </SafeAreaView>
-  )}
+  );
+}
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
