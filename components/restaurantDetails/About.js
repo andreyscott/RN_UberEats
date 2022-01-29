41a1678d5345225f48192a1ecffe07a250f00489
andreyscott <andreyscott>
2022-01-29 11:35:47 +0100

    let there be light

--- components/restaurantDetails/About.js
+++ components/restaurantDetails/About.js
@@ -2,7 +2,7 @@ import { View, Text, Image } from 'react-native';
 import React from 'react';
 
 const image = 
-''
+'https://images.unsplash.com/photo-1552566626-52f8b828add9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
 
 const title = 'Farmhouse Kitchen Restaurant';
   const address = '123 Main Street, New York, NY 10012';
@@ -10,10 +10,9 @@ const title = 'Farmhouse Kitchen Restaurant';
 export default function About() {
   return (
     <View>
-      <RestaurantImage image={image} />
-      <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10 }}>
-        {title}
-      </Text>
+      {/* <RestaurantImage image={image} /> */}
+      <RestaurantTitle title={title} />
+      <RestaurantDescription description={description} />
     </View>
   );
 }
@@ -24,3 +23,29 @@ const RestaurantImage = (props) => (
    />
 
 );
+
+const RestaurantTitle = (props) => {
+  <Text style={{
+    fontSize: 29,
+    fontWeight: 600,
+    marginTop:10,
+    marginHorizontal: 15,
+
+  }}>
+  {props.title}
+
+  </Text>
+}
+
+const RestaurantDescription = (props) => (
+  <Text
+  style={{
+    fontWeight: 400,
+    marginTop: 10,
+    marginHorizontal: 15,
+    fontSize:14.5,
+  }}
+  >
+    {props.description}
+  </Text>
+)
---
+++ components/restaurantDetails/MenuItem.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function MenuItem() {
+  return (
+    <View>
+      <Text>
+          Menu Items 😍
+      </Text>
+    </View>
+  );
+}
